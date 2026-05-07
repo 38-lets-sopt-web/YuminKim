3f936f0f2812325d32e970f55328b82670764fc5
yumin-kim4757 <rladbals0211@gmail.com>
2026-05-08 05:32:16 +0900

    feat: ThemeProvider 구현 #8

---
+++ Week4/Homework/src/styles/theme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  color: {
+    primary: "#eaf2f9",
+    text: "#173a5e",
+    border: "#cbd5e1",
+    button: "#aeefff",
+    buttonHover: "#79ddf0",
+    buttonText: "#2563eb",
+  },
+  font: {
+    title: "1.75rem",
+    label: "0.8125rem",
+    input: "0.875rem",
+    button: "0.9375rem",
+    caption: "0.8125rem",
+  },
+};
