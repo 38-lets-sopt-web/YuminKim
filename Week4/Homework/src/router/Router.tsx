f44b159aa7720c0d6485b60517b4a3e0e5f43353
yumin-kim4757 <rladbals0211@gmail.com>
2026-05-08 19:37:55 +0900

    feat: / path 추가 #15

--- Week4/Homework/src/router/Router.tsx
+++ Week4/Homework/src/router/Router.tsx
@@ -2,9 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import LoginPage from "../pages/login/LoginPage";
 import SignupPage from "../pages/sign-up/SignupPage";
 import Mypage from "../pages/mypages/MyPage";
+import { Navigate } from "react-router";
 
 function Router() {
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navigate to="/login" replace />,
+    },
     {
       path: "/login",
       element: <LoginPage />,
