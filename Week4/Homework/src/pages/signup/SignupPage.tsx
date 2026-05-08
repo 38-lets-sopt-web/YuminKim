3f2e85cf247ba2ae887de61c2ec7f6af117123dd
yumin-kim4757 <rladbals0211@gmail.com>
2026-05-08 18:19:23 +0900

    feat: 회원가입 입력 상태 관리 #12

--- Week4/Homework/src/pages/signup/SignupPage.tsx
+++ Week4/Homework/src/pages/signup/SignupPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router";
 import {
   PageContainer,
@@ -12,6 +13,30 @@ import Input from "../../components/input/Input";
 
 function SignupPage() {
   const navigate = useNavigate();
+  const [signupForm, setSignupForm] = useState({
+    username: "",
+    password: "",
+    name: "",
+    email: "",
+    age: "",
+    part: "",
+  });
+
+  const handleSignupFormChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const { id, value } = event.target;
+
+    setSignupForm({
+      ...signupForm,
+      [id]: value,
+    });
+  };
+
+  const handleSignupButtonClick = () => {
+    console.log(signupForm);
+    alert("회원가입에 성공했습니다!");
+  };
 
   return (
     <PageContainer>
@@ -22,6 +47,8 @@ function SignupPage() {
           id="username"
           label="아이디"
           placeholder="아이디를 입력해주세요."
+          value={signupForm.username}
+          onChange={handleSignupFormChange}
         />
 
         <Input
@@ -29,17 +56,47 @@ function SignupPage() {
           label="비밀번호"
           type="password"
           placeholder="비밀번호를 입력해주세요."
+          value={signupForm.password}
+          onChange={handleSignupFormChange}
         />
 
-        <Input id="name" label="이름" placeholder="이름을 입력해주세요." />
+        <Input
+          id="name"
+          label="이름"
+          placeholder="이름을 입력해주세요."
+          value={signupForm.name}
+          onChange={handleSignupFormChange}
+        />
 
-        <Input id="email" label="이메일" placeholder="이메일을 입력해주세요." />
+        <Input
+          id="email"
+          label="이메일"
+          type="email"
+          placeholder="이메일을 입력해주세요."
+          value={signupForm.email}
+          onChange={handleSignupFormChange}
+        />
 
-        <Input id="age" label="나이" placeholder="나이를 입력해주세요." />
+        <Input
+          id="age"
+          label="나이"
+          type="number"
+          placeholder="나이를 입력해주세요."
+          value={signupForm.age}
+          onChange={handleSignupFormChange}
+        />
 
-        <Input id="part" label="파트" placeholder="파트를 입력해주세요." />
+        <Input
+          id="part"
+          label="파트"
+          placeholder="파트를 입력해주세요."
+          value={signupForm.part}
+          onChange={handleSignupFormChange}
+        />
 
-        <SignupButton type="button">회원가입</SignupButton>
+        <SignupButton type="button" onClick={handleSignupButtonClick}>
+          회원가입
+        </SignupButton>
 
         <LoginText>
           이미 계정이 있나요?
